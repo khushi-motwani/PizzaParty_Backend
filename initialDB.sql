4d64f4e23329de2347cca03f534500689c134de3
Renee Knapp <renee.f.knapp@gmail.com>
2026-07-22 11:09:53 +0000

    Added AssetPrice Table and balance details to Transaction Table

--- initialDB.sql
+++ initialDB.sql
@@ -14,6 +14,16 @@ CREATE TABLE Assets
    PRIMARY KEY(asset_id)
 );
 
+CREATE TABLE AssetPrices
+(
+   asset_id          int               NOT NULL,
+   price             decimal(14,4)     NOT NULL, --sub-dollar can be quoted to 4 decimal places
+   as_of_timestamp   datetime          NOT NULL,
+
+   PRIMARY KEY(asset_id, as_of_timestamp),
+   FOREIGN KEY(asset_id) REFERENCES Assets(asset_id)
+);
+
 CREATE TABLE Portfolios
 (
    portfolio_id          int 	           auto_increment,
@@ -26,11 +36,13 @@ CREATE TABLE Transactions
 (
    transaction_id    int               auto_increment,
    portfolio_id      int               NOT NULL,
-   asset_id          int 	           NOT NULL,
+   asset_id          int,     -- facilitate transaction types of withdrawal/deposit
    transaction_type  varchar(20)       NOT NULL DEFAULT 'UNKNOWN',
    transaction_quantity  int           NOT NULL DEFAULT 0,
-   transaction_price     int           NOT NULL DEFAULT 0,
-   transaction_date      date          NOT NULL,
+   transaction_price     decimal(14,4),
+   transaction_date      datetime      NOT NULL,
+   transaction_total     decimal(14,2) NOT NULL DEFAULT 0,
+   balance_after_transaction  decimal(14,2) NOT NULL DEFAULT 0,
 
    PRIMARY KEY(transaction_id),
    FOREIGN KEY(portfolio_id) REFERENCES Portfolios(portfolio_id),
