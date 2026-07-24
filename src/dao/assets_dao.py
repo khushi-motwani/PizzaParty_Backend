4b0ebab557995d2eb7978f160d3f73b0ed9fbb44
Vilmos Toth <vilmos.toth@morganstanley.com>
2026-07-24 14:24:58 +0000

    Added fix to dao tests

--- src/dao/assets_dao.py
+++ src/dao/assets_dao.py
@@ -2,14 +2,19 @@ from dao.db_config import get_db_connection
 from dto.assets_dto import AssetsDTO
 
 class AssetsDao:
-    def __init__(self):
-        self.connection = get_db_connection()
+    def __init__(self, connection_factory=None):
+        self.connection_factory = connection_factory or get_db_connection
+        self.connection = None
         self.assets = []
         self.total = 0
 
+    def _get_connection(self):
+        if self.connection is None:
+            self.connection = self.connection_factory()
+        return self.connection
 
     def count(self):
-        dbcursor = self.connection.cursor()
+        dbcursor = self._get_connection().cursor()
         dbcursor.execute("SELECT count(*) as Total FROM Assets")
         result = dbcursor.fetchall()
         self.total = result[0][0]
@@ -17,7 +22,7 @@ class AssetsDao:
 
 
     def get_all(self):
-        dbcursor = self.connection.cursor()
+        dbcursor = self._get_connection().cursor()
         dbcursor.execute("SELECT * FROM Assets")
         result = dbcursor.fetchall()
 
